63853bc379c26b3e9f713f85aa6ffcdc8a2537ba
Gianpiero Addis <gianpiero.addis@gmail.com>
2018-08-30 20:06:40 +0200

    Add ecomdev_phpunit test

---
+++ app/code/local/Namespace/ModuleName/Test/Model/Foo.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @category   Namespace
+ * @package    Namespace_ModuleName
+ * @author     authorName <authorEmail>
+ */
+class Namespace_ModuleName_Test_Model_Foo extends EcomDev_PHPUnit_Test_Case
+{
+    // Code...
+}
