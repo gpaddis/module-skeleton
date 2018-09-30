ee8be7314ef90ac694d3751bee66f240abc3e320
Gianpiero Addis <gianpiero.addis@gmail.com>
2018-09-30 17:09:04 +0200

    Add an example test to avoid breaking test suite

--- app/code/local/Namespace/ModuleName/Test/Model/Foo.php
+++ app/code/local/Namespace/ModuleName/Test/Model/Foo.php
@@ -7,5 +7,9 @@
  */
 class Namespace_ModuleName_Test_Model_Foo extends EcomDev_PHPUnit_Test_Case
 {
-    // Code...
+    /** @test */
+    public function exampleTest()
+    {
+        $this->assertTrue(true);
+    }
 }
