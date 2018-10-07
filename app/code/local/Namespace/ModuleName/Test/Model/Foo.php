26f365caf6aa1b48fd65c4193f4e740a8a5491fa
Gianpiero Addis <gianpiero.addis@gmail.com>
2018-10-07 10:13:00 +0200

    Add @group annotation in the test class

--- app/code/local/Namespace/ModuleName/Test/Model/Foo.php
+++ app/code/local/Namespace/ModuleName/Test/Model/Foo.php
@@ -4,10 +4,15 @@
  * @category   Namespace
  * @package    Namespace_ModuleName
  * @author     authorName <author@email.net>
+ *
+ * PHPUnit Class Annotations
+ * @group Namespace_ModuleName
  */
 class Namespace_ModuleName_Test_Model_Foo extends EcomDev_PHPUnit_Test_Case
 {
-    /** @test */
+    /**
+     * @test
+     */
     public function exampleTest()
     {
         $this->assertTrue(true);
