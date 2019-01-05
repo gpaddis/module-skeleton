5c9c826ce6cb8d41c681b3875e4691b4332b2734
Gianpiero Addis <gianpiero.addis@gmail.com>
2019-01-05 10:26:48 +0100

    Improve test description, rename test

--- app/code/local/Namespace/ModuleName/Test/Model/Foo.php
+++ app/code/local/Namespace/ModuleName/Test/Model/Foo.php
@@ -13,13 +13,13 @@ class Namespace_ModuleName_Test_Model_Foo extends EcomDev_PHPUnit_Test_Case
 {
     /**
      * @test
+     *
+     * The test checks much more than just the current date: the module Foo imports
+     * the Carbon library thanks to the module firegento/psr0autoloader, and the
+     * forked testing module MageTestStand installs the dependencies during the
+     * build process, so that they are available when the test is run.
      */
-    public function exampleTest()
-    {
-        $this->assertTrue(true);
-    }
-
-    public function testItReturnsTheCurrentDate()
+    public function it_returns_the_current_date()
     {
         $foo = Mage::getModel('namespace_modulename/foo');
         $today = Carbon::now()->toDateString();
